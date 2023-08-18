cd7004efe08f849609c784316a5f686d7e267c49
sharontan6217 <52969503+sharontan6217@users.noreply.github.com>
2023-08-18 19:31:41 +0800

    Update autocalculator.py

--- autocalculator.py
+++ autocalculator.py
@@ -3,6 +3,6 @@ import calculator
 class TestCal:
     def test_multiple(self):
         assert 4 == calculator.multiple(2,2)
-    def test_divid(self):
+    def test_divide(self):
         assert 4 == calculator.divide(8,2)
         
