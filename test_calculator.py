569785d52db3054d2218838fea8186eeee703d47
sharontan6217 <52969503+sharontan6217@users.noreply.github.com>
2023-10-08 20:18:03 +0800

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -4,6 +4,6 @@ import calculator
 class TestCalculator:
     def test_multiple(self):
         assert 4 == calculator.multiple(2, 2)
-        
+
     def test_divide(self):
         assert 4 == calculator.divide(8, 2)
